b828d17b72c6bb9c3f6d50adc2308f7788b64f3e
hemantsingh0557 <hemantsingh0557@gmail.com>
2025-01-05 23:04:21 +0530

    ch11

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const startServer = async() => {
     await dbConnection() ;
     await redisConnection(); 
     // await expressStartup(app) ;
-    app.listen(config.server.port , ()=> {
-        console.log(`Server is running on http://localhost:${config.server.port}`);
-    });
+    // app.listen(config.server.port , ()=> {
+    //     console.log(`Server is running on http://localhost:${config.server.port}`);
+    // });
 };
 
 startServer().catch((error) => {
