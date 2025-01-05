ad756afa5a4a63286c53c12811c5b34d94a504bd
hemantsingh0557 <hemantsingh0557@gmail.com>
2025-01-05 23:27:52 +0530

    22

--- startup/expressStartup.js
+++ startup/expressStartup.js
@@ -1,42 +1,42 @@
 // import express from "express";
-// import { allRoutes } from "../routes/index.js"; 
-// import { helperFunctions } from "../utils/helperFunctions.js";
-// import { authenticateToken } from "../middleware/index.js";
+import { allRoutes } from "../routes/index.js"; 
+import { helperFunctions } from "../utils/helperFunctions.js";
+import { authenticateToken } from "../middleware/index.js";
 // import { authRoutes } from "../routes/authRoute.js"; // Import your routes
 // import passport from "../config/passport.js"; 
 // import session from "express-session";
 // import config from "../config/index.js";
-// import { UAParser } from "ua-parser-js";
+import { UAParser } from "ua-parser-js";
 
-// const handler = (controller) => {
-//     return (req, res) => {
-//         const userAgentString = req.headers["user-agent"];
-//         const userIpAddress = req.headers["x-forwarded-for"] || req.ip;
-//         const uaParser = new UAParser();
-//         const userAgentData = uaParser.setUA(userAgentString).getResult();
-//         const userDeviceType = userAgentData.device.type || "desktop"; // Mobile, tablet, or desktop
-//         const userOsType = userAgentData.os.name || "Unknown"; //  Windows, Android, macOS, iOS
-//         const payload = {
-//             ...(req.body || {}),
-//             ...(req.params || {}),
-//             ...(req.query || {}),
-//             userId: req.userId, 
-//             userIpAddress, 
-//             userDeviceType, 
-//             userOsType, 
-//         };
-//         controller(payload)
-//             .then(async(result) => {
-//                 if (result.data && result.data.redirectUrl) {
-//                     return res.redirect(result.data.redirectUrl);
-//                 }
-//                 res.status(result.statusCode).json(result);
-//             })
-//             .catch(async(error) => {
-//                 res.status(error.statusCode || 500).json(error);
-//             });
-//     };
-// };
+const handler = (controller) => {
+    return (req, res) => {
+        const userAgentString = req.headers["user-agent"];
+        const userIpAddress = req.headers["x-forwarded-for"] || req.ip;
+        const uaParser = new UAParser();
+        const userAgentData = uaParser.setUA(userAgentString).getResult();
+        const userDeviceType = userAgentData.device.type || "desktop"; // Mobile, tablet, or desktop
+        const userOsType = userAgentData.os.name || "Unknown"; //  Windows, Android, macOS, iOS
+        const payload = {
+            ...(req.body || {}),
+            ...(req.params || {}),
+            ...(req.query || {}),
+            userId: req.userId, 
+            userIpAddress, 
+            userDeviceType, 
+            userOsType, 
+        };
+        controller(payload)
+            .then(async(result) => {
+                if (result.data && result.data.redirectUrl) {
+                    return res.redirect(result.data.redirectUrl);
+                }
+                res.status(result.statusCode).json(result);
+            })
+            .catch(async(error) => {
+                res.status(error.statusCode || 500).json(error);
+            });
+    };
+};
 
 export async function expressStartup(app) {
     // app.use(session({
@@ -54,11 +54,11 @@ export async function expressStartup(app) {
         res.send("This is a express test project.OkOkOkOkOkOkOk");
     });
 
-    // allRoutes.forEach((route) => {
-    //     const { method, path, schema = {}, auth = false, controller } = route;
-    //     const middleware = [];
-    //     if (schema) { middleware.push(helperFunctions.validateSchema(schema)); }
-    //     if (auth) { middleware.push(authenticateToken); }
-    //     app[method](path, ...middleware, handler(controller));
-    // });
+    allRoutes.forEach((route) => {
+        const { method, path, schema = {}, auth = false, controller } = route;
+        const middleware = [];
+        if (schema) { middleware.push(helperFunctions.validateSchema(schema)); }
+        if (auth) { middleware.push(authenticateToken); }
+        app[method](path, ...middleware, handler(controller));
+    });
 }
