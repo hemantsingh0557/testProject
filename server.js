9791c14ae3c01dd4084d3c743dc3d1dd1b09b990
hemantsingh0557 <hemantsingh0557@gmail.com>
2025-01-05 23:09:09 +0530

    13

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-
+import express from "express" ;
 import { dbConnection } from "./startup/dbConnection.js";
-import app from "./startup/expressStartup.js";
+// import app from "./startup/expressStartup.js";
 import config from "./config/index.js";
 import { redisConnection } from "./startup/redisConnection.js";
 
-
+const app = express() ;
+app.get("/" , (req , res) => {
+    res.send("this si text ptoejcjksdshfshdbsm") ;
+});
 
 const startServer = async() => {
     await dbConnection() ;
     await redisConnection(); 
     // await expressStartup(app) ;
-    // app.listen(config.server.port , ()=> {
-    //     console.log(`Server is running on http://localhost:${config.server.port}`);
-    // });
+    app.listen(config.server.port , ()=> {
+        console.log(`Server is running on http://localhost:${config.server.port}`);
+    });
 };
 
 startServer().catch((error) => {
