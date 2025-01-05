19d91882523cea725e199e5d1e7380202c86d309
hemantsingh0557 <hemantsingh0557@gmail.com>
2025-01-06 00:00:00 +0530

    41

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,54 +1,54 @@
 
-// import jwt from "jsonwebtoken" ; 
-// import config from "../config/index.js";
+import jwt from "jsonwebtoken" ; 
+import config from "../config/index.js";
 import geoip from "geoip-lite";
 
 export const helperFunctions = {} ;
 
 
-// helperFunctions.generateJWTAccessToken = (jwtPayloadObject) => {
-//     return jwt.sign(jwtPayloadObject, config.auth.jwtSecret , { algorithm: "HS256" , expiresIn: "1h" });
-// };
+helperFunctions.generateJWTAccessToken = (jwtPayloadObject) => {
+    return jwt.sign(jwtPayloadObject, config.auth.jwtSecret , { algorithm: "HS256" , expiresIn: "1h" });
+};
 
 
 
-// helperFunctions.validateSchema = (schema) => {
-//     return (req , res , next) => {
-//         if (schema.params) {
-//             const { error } = schema.params.validate(req.params);
-//             if (error) {
-//                 return res.status(400).json({
-//                     error: error.details.map((err) => err.message),
-//                 });
-//             }
-//         }
-//         if (schema.body) {
-//             const { error } = schema.body.validate(req.body );
-//             if (error) {
-//                 return res.status(400).json({
-//                     error: error.details.map((err) => err.message),
-//                 });
-//             }
-//         }
-//         if (schema.query) {
-//             const { error } = schema.query.validate(req.query);
-//             if (error) {
-//                 return res.status(400).json({
-//                     error: error.details.map((err) => err.message),
-//                 });
-//             }
-//         }
-//         if (schema.headers) {
-//             const { error } = schema.headers.validate(req.headers );
-//             if (error) {
-//                 return res.status(400).json({
-//                     error: error.details.map((err) => err.message),
-//                 });
-//             }
-//         }
-//         next();
-//     } ;
-// } ;
+helperFunctions.validateSchema = (schema) => {
+    return (req , res , next) => {
+        if (schema.params) {
+            const { error } = schema.params.validate(req.params);
+            if (error) {
+                return res.status(400).json({
+                    error: error.details.map((err) => err.message),
+                });
+            }
+        }
+        if (schema.body) {
+            const { error } = schema.body.validate(req.body );
+            if (error) {
+                return res.status(400).json({
+                    error: error.details.map((err) => err.message),
+                });
+            }
+        }
+        if (schema.query) {
+            const { error } = schema.query.validate(req.query);
+            if (error) {
+                return res.status(400).json({
+                    error: error.details.map((err) => err.message),
+                });
+            }
+        }
+        if (schema.headers) {
+            const { error } = schema.headers.validate(req.headers );
+            if (error) {
+                return res.status(400).json({
+                    error: error.details.map((err) => err.message),
+                });
+            }
+        }
+        next();
+    } ;
+} ;
 
 const BASE62_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 helperFunctions.generateRandomString = (length= 8) => {
